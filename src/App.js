dd493da714a6aea04a5cf85ba8352f2f6a0455ef
omer9706 <omer_yurtseven1997@outlook.com>
2023-05-01 11:45:49 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Components/Card";
+import HomePageImage from "./Components/Images/bg.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row h-screen overflow-x-hidden relative">
+      <img src={HomePageImage} alt="Home Page" className="absolute z-[-1] w-full h-[437px] object-fill xl:z-40 xl:static xl:h-full" />
+      <div className="flex w-full pt-56 flex-col pl-7 xl:pl-36 xl:w-1/2 h-full z-20 xl:pt-[96px]">
+        <h1 className="text-white text-xl pr-40 w-full xl:text-4xl font-custom xl:text-black items-center font-bold xl:pr-[300px] xl:min-w-[200px]">Everyday items, we have something to suit every occasion.</h1>
+        <p className="hidden font-avenir xl:block xl:pt-[12px] text-[#666666] pr-[400px]">At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit tincidunt semper eu proin leo gravida cursus.</p>
+        <button className="flex font-avenir flex-col w-48 mt-8 items-center text-white xl:text-black font-bold xl:mt-10">
+          Shop All Everyday Items
+          <hr className="mt-1 w-full border-white xl:border-black" />
+        </button>
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,87 @@
+import React, {useRef,useState } from "react";
+import itemListImgFirst from "./Images/bg-1.png";
+import itemListImgSecond from "./Images/bg-2.png";
+import itemListImgThird from "./Images/bg-3.png";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import { motion, useDragControls } from "framer-motion";
+
+const itemLists = [
+  {
+    img: itemListImgFirst,
+    title: "365 Signature Hoodie",
+    price: "€33.95",
+  },
+  {
+    img: itemListImgThird,
+    title: "Organic Skinny High Waist Jeans",
+    price: "€33.95",
+  },
+  {
+    img: itemListImgSecond,
+    title: "Organic Skinny High Waist Jeans",
+    price: "€33.95",
+  },
+];
+
+function Card() {
+  const controls = useDragControls();
+  const sliderRef = useRef(null);
+  const [position, setPosition] = useState(0);
+
+  console.log(position);
+
+  const handleArrowClick = () => {
+    setPosition(-200);
+  };
+
+  function ColorChanger() {
+    const [indexColor,setColor]=useState(0);
+
+    const colors=["#99C3CC","#CC9999","#CB99CC","#A6CC99"];
+
+    return (
+      <div className="flex flex-row gap-2 justify-end pr-1 pt-0.5">
+        {colors.map((color, index) => (
+          <div key={index} onClick={()=>setColor(index)} className={`${indexColor===index?"border-2 border-black":"border-2 border-white"} rounded-full w-3 h-3 cursor-pointer xl:w-4 xl:h-4`} style={{backgroundColor:color}}></div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-row pt-5 xl:pt-[68px] h-full relative">
+      <motion.div
+        className="flex flex-row gap-2 z-20 gap-3 xl:gap-[32px] "
+        drag="x"
+        dragConstraints={{ left: -200, right: 0 }}
+        dragElastic={0}
+        dragMomentum={false}
+        ref={sliderRef}
+        dragControls={controls}
+        style={{ x: position }} // Set x-axis position based on state value
+      >
+        {itemLists.map((item, index) => (
+          <div key={index} className="flex flex-col gap-2 font-avenir font-normal text-xs leading-none w-[149px] h-[292px] xl:w-[230px] xl:h-[363px]">
+            <img
+              src={item.img}
+              alt={item.title}
+              className="w-[230px] h-[310px] object-cover"
+            />
+            <h3 className="text-[#1D1F22] text-lg">{item.title}</h3>
+            <div className="flex">
+              <p className="text-[#1D1F22] text-[18px] font-bold flex-1">{item.price}</p>
+              <ColorChanger />
+            </div>
+          </div>
+        ))}
+      </motion.div>
+      <ArrowForwardIosRoundedIcon
+        fontSize="xl"
+        className="hidden p-2 border border-gray-500 text-[#1D1F22] text-4xl z-40 top-56 right-16  xl:absolute"
+        onClick={handleArrowClick} // Call handleArrowClick function on arrow click
+      />
+    </div>
+  );
+}
+
+export default Card;
